6a0906af42cab69ebcaccf59b319f31aa79af982
NirupanJoe <nirupanjoej@gmail.com>
2021-09-15 09:49:10 +0000

    Missed file.

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -3,7 +3,8 @@ const controller = require('../controller/todoController');
 
 const router = express.Router();
 
-router.route('/').get(controller.allTodo).post(controller.createTodo);
-router.route('/:todo_id').delete(controller.deleteTodo).put(controller.updateTodo);
+router.route('/').get(controller.listTodo).post(controller.createTodo);
+router.route('/:todo_id').delete(controller.deleteTodo).put(controller.updateTodo)
+    .get(controller.getTodo);
 
 module.exports = router;
